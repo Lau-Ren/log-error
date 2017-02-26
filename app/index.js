bde3da6aaa6bae2462cf6d3291dfe9a3e8aebd99
Lauren Colls <lauren.colls@vizexplorer.com>
2017-02-27 10:30:19 +1300

    added thingy at top

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 var fs = require('fs');
 var clc = require("cli-color");
 var prompt = require('prompt');
 
-console.log('in');
+
 prompt.start();
 
 prompt.get(['Error', 'Solution', 'Tags'], function(err, res){
